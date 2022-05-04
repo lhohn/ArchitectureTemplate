2238c1f059d303ae0cacbe912b48f81fe7770a44
lhohn2s <luis-maria.hohn@smail.inf.h-brs.de>
2022-05-04 13:27:18 +0200

    add primeng support

--- source/Web/Frontend/src/app/layouts/layouts.module.ts
+++ source/Web/Frontend/src/app/layouts/layouts.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { AppFooterComponent } from "./footer/footer.component";
 import { AppHeaderComponent } from "./header/header.component";
 import { AppLayoutMainComponent } from "./layout-main/layout-main.component";
 import { AppLayoutComponent } from "./layout/layout.component";
 import { AppNavComponent } from "./nav/nav.component";
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
     declarations: [
@@ -13,7 +14,12 @@ import { AppNavComponent } from "./nav/nav.component";
         AppLayoutComponent,
         AppLayoutMainComponent,
         AppNavComponent
+        
     ],
-    imports: [RouterModule]
+    imports: [
+        RouterModule,
+        MenubarModule
+    ],
+    schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppLayoutsModule { }
